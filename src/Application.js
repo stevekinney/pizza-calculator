d9e2bb283a15bf3740767cdf64d5dfc78b64e99b
Steve Kinney <hello@stevekinney.net>
2017-11-16 14:08:11 -0600

    Add container pattern

--- src/Application.js
+++ src/Application.js
@@ -11,7 +11,43 @@ const initialState = {
   slicesPerPerson: 2,
 };
 
-export default class Application extends Component {
+class PizzaCalculator extends Component {
+  render() {
+    const {
+      numberOfPeople,
+      updateNumberOfPeople,
+      slicesPerPerson,
+      updateSlicesPerPerson,
+      reset,
+      numberOfPizzas,
+    } = this.props;
+    return (
+      <div className="Application">
+        <Title />
+        <Input
+          label="Number of Guests"
+          type="number"
+          min={0}
+          value={numberOfPeople}
+          onChange={updateNumberOfPeople}
+        />
+        <Input
+          label="Slices Per Person"
+          type="number"
+          min={0}
+          value={slicesPerPerson}
+          onChange={updateSlicesPerPerson}
+        />
+        <Result amount={numberOfPizzas} />
+        <button className="full-width" onClick={reset}>
+          Reset
+        </button>
+      </div>
+    );
+  }
+}
+
+class WithPizzaCalculations extends Component {
   state = { ...initialState };
 
   updateNumberOfPeople = event => {
@@ -36,27 +72,22 @@ export default class Application extends Component {
     );
 
     return (
-      <div className="Application">
-        <Title />
-        <Input
-          label="Number of Guests"
-          type="number"
-          min={0}
-          value={numberOfPeople}
-          onChange={this.updateNumberOfPeople}
-        />
-        <Input
-          label="Slices Per Person"
-          type="number"
-          min={0}
-          value={slicesPerPerson}
-          onChange={this.updateSlicesPerPerson}
-        />
-        <Result amount={numberOfPizzas} />
-        <button className="full-width" onClick={this.reset}>
-          Reset
-        </button>
-      </div>
+      <PizzaCalculator
+        numberOfPeople={numberOfPeople}
+        updateNumberOfPeople={this.updateNumberOfPeople}
+        slicesPerPerson={slicesPerPerson}
+        updateSlicesPerPerson={this.updateSlicesPerPerson}
+        numberOfPizzas={numberOfPizzas}
+        reset={this.reset}
+      />
     );
+  }
+}
+
+// const PizzaContainer = WithPizzaCalculations(PizzaCalculator);
+
+export default class Application extends Component {
+  render() {
+    return <WithPizzaCalculations />;
   }
 }
