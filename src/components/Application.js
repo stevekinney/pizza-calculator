2b9b385c6c59ab3e78ab6b851c36a19eb92405ed
Steve Kinney <hello@stevekinney.net>
2017-11-17 09:17:52 -0600

    Get set up for the base

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,18 +1,10 @@
 import React from 'react';
 
 import Title from './Title';
-import NumberOfPeopleInputContainer from '../containers/NumberOfPeopleInputContainer';
-import SlicesPerPersonInputContainer from '../containers/SlicesPerPersonInputContainer';
-import ResultContainer from '../containers/ResultContainer';
-import ResetButtonContainer from '../containers/ResetButtonContainer';
 
 const Application = () => (
   <div className="Application">
     <Title />
-    <NumberOfPeopleInputContainer />
-    <SlicesPerPersonInputContainer />
-    <ResultContainer />
-    <ResetButtonContainer />
   </div>
 );
 
--- src/containers/ResultContainer.js
+++
@@ -1,8 +0,0 @@
-import { connect } from 'react-redux';
-import Result from '../components/Result';
-
-const mapStateToProps = ({ numberOfPizzasNeeded }) => {
-  return { amount: numberOfPizzasNeeded };
-};
-
-export default connect(mapStateToProps, null)(Result);
