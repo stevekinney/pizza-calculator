5cb89225bfe2e1e2a1c301c2be69fb64b426867d
Rolis <rolandas-kairys@users.noreply.github.com>
2017-12-11 18:10:26 +0200

    minor changes to glue missing pices in Application.js

--- src/Application.js
+++ src/Application.js
@@ -9,13 +9,9 @@ import * as actions from './actions';
 
 import calculatePizzasNeeded from './lib/calculate-pizzas-needed';
 
-const initialState = {
-  numberOfPeople: 10,
-  slicesPerPerson: 2,
-};
 
 class PizzaCalculator extends Component {
-  render () {
+  render(){
     return (
       <div className="Application">
       <Title />
@@ -23,18 +19,18 @@ class PizzaCalculator extends Component {
         label="Number of Guests"
         type="number"
         min={0}
-        value={numberOfPeople}
-        onChange={updateNumberOfPeople}
+        value={ this.props.numberOfPeople }
+        onChange={this.props.updateNumberOfPeople}
       />
       <Input
         label="Slices Per Person"
         type="number"
         min={0}
-        value={slicesPerPerson}
-        onChange={updateSlicesPerPerson}
+        value={this.props.slicesPerPerson}
+        onChange={this.props.updateSlicesPerPerson}
       />
-      <Result amount={numberOfPizzas} />
-      <button className="full-width" onClick={reset}>
+      <Result amount={this.props.numberOfPizzas} />
+      <button className="full-width" onClick={this.props.reset}>
         Reset
       </button>
     </div>
@@ -43,11 +39,12 @@ class PizzaCalculator extends Component {
 }
 
 export default class Application extends Component {
-  state = PizzaCalculator.getState();
+
+  state = PizzaCalculatorStore.getState();
 
   updateNumberOfPeople = event => {
     const numberOfPeople = parseInt(event.target.value, 10);
-    actions.updateNumberOfPeople(numberOfPeople);
+    actions.updateNumberOfpeople(numberOfPeople);
   };
 
   updateSlicesPerPerson = event => {
@@ -55,17 +52,22 @@ export default class Application extends Component {
     actions.updateSlicesPerPerson(slicesPerPerson);
   };
 
-  updateState() {
-    this.setState(PizzaCalculatorStore.getState());
-  }
 
-  componentDidMount() {
+
+  componentDidMount = () => {
     PizzaCalculatorStore.on('change', this.updateState);
   }
 
-  componentWillUnmount() {
+  componentWillUnmount = () => {
     PizzaCalculatorStore.off('change', this.updateState);
   }
+
+
+  updateState = () => {
+    this.setState( PizzaCalculatorStore.getState() );
+  }
+
+
 
   render() {
     const { numberOfPeople, slicesPerPerson } = this.state;
@@ -75,8 +77,9 @@ export default class Application extends Component {
     );
 
     return (
-      <PizzaCalculator
-        {...this.state}
+      <PizzaCalculator 
+        numberOfPeople = { numberOfPeople }
+        slicesPerPerson={slicesPerPerson}
         numberOfPizzas={numberOfPizzas}
         updateNumberOfPeople={this.updateNumberOfPeople}
         updateSlicesPerPerson={this.updateSlicesPerPerson}
